cc0ff35d820a674ac80f78d22f0713917c514f0e
Kiong <letan@sephora.sg>
2021-03-29 17:48:52 +0800

    change require_relative to load

--- db/seeds_v2/development/brands/default.seeds.rb
+++ db/seeds_v2/development/brands/default.seeds.rb
@@ -1,4 +1,4 @@
-require_relative "../../helpers/brands"
+load Rails.root.join("db/seeds_v2/helpers/brands.rb")
 
 ['Dev default brand 1', 'Dev default brand 2', 'Dev default brand 3'].each do |name|
   attr = {
